f9fffb206b96c7ce04a611d75a04a290bb0968c7
Sebastien Cros <secros@z3r6p5.42lyon.fr>
2025-03-30 19:35:55 +0200

    fix: Delayed status --need to check for philo state in another thread to not slow down all the exiting one

--- src/routine/action.c
+++ src/routine/action.c
@@ -6,7 +6,7 @@
 /*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 18:08:29 by secros            #+#    #+#             */
-/*   Updated: 2025/03/30 18:48:38 by secros           ###   ########.fr       */
+/*   Updated: 2025/03/30 19:35:05 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@ int	sleeping(t_philo *philo)
 {
 	size_t	time;
 
+	if (is_a_philo_dead(philo))
+		return (1);
 	time = 0;
 	pthread_mutex_lock(&philo->lock->printing);
 	printf("%s%lld philo %d is sleeping\n%s", FG_BLUE, \
 		get_delta(philo->param->start), philo->philo, RESET);
 	pthread_mutex_unlock(&philo->lock->printing);
-	return (micro_sleep(philo, philo->param->t_sleep));
+	usleep(philo->param->t_sleep);
+	return (0);
 }
 
 void	thinking(t_philo *philo)
@@ -36,7 +39,7 @@ int	try_to_eat(t_philo *philo)
 {
 	while (1)
 	{
-		if (is_a_philo_dead(philo) || is_dead(philo))
+		if (is_a_philo_dead(philo))
 			return (1);
 		if (check_fork(philo))
 		{
@@ -46,8 +49,7 @@ int	try_to_eat(t_philo *philo)
 			pthread_mutex_unlock(&philo->lock->printing);
 			if (gettimeofday(&philo->last_meal, NULL) != 0)
 				return (2); //Not securized
-			if (micro_sleep(philo, philo->param->t_eat) != 0)
-				return (1);
+			usleep(philo->param->t_eat * 1000);
 			fork_unlock(philo);
 			philo->eaten++;
 			break ;
